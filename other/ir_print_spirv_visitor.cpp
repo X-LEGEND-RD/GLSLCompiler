1891e827c92f1e5706da45043fa1106e777d66ba
crueleva1 <kangmilm@gmail.com>
2017-08-17 16:02:41 +0800

    Fix AccessChain with wrong result type

--- other/ir_print_spirv_visitor.cpp
+++ other/ir_print_spirv_visitor.cpp
@@ -1449,13 +1449,14 @@ void ir_print_spirv_visitor::visit(ir_dereference_array *ir)
    visit_value(ir->array_index);
 
    unsigned int type_id = visit_type(ir->type);
-   unsigned int type_id_pointer = visit_type_pointer(ir->type, ir_var_auto, type_id);
+   
    unsigned int return_id = f->id++;
    if (ir->array->ir_uniform) {
       ir_constant ir_uniform(ir->array->ir_uniform - 1);
       ir_uniform.ir_value = 0;
       visit(&ir_uniform);
 
+      unsigned int type_id_pointer = visit_type_pointer(ir->type, ir_var_uniform, type_id);
       f->functions.push(SpvOpAccessChain | (6 << SpvWordCountShift));
       f->functions.push(type_id_pointer);
       f->functions.push(return_id);
@@ -1465,6 +1466,7 @@ void ir_print_spirv_visitor::visit(ir_dereference_array *ir)
 
       ir->array->ir_pointer = return_id;
    } else {
+      unsigned int type_id_pointer = visit_type_pointer(ir->type, ir_var_auto, type_id);
       f->functions.push(SpvOpAccessChain | (5 << SpvWordCountShift));
       f->functions.push(type_id_pointer);
       f->functions.push(return_id);
